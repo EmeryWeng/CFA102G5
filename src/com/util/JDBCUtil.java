f8e2e300c2a99b53332dcc00d5d4eb8cd7670798
xinweiWeng <dybleyer0421@gmail.com>
2021-08-20 23:54:55 +0800

    JDBCDemo

--- src/com/util/JDBCUtil.java
+++ src/com/util/JDBCUtil.java
@@ -4,4 +4,5 @@ public class JDBCUtil {
 	public static final String url = "jdbc:mysql://localhost:3306/cfa102_g5?serverTimezone=UTC";
 	public static final String username = "David";
 	public static final String password = "123456";
+	
 }
