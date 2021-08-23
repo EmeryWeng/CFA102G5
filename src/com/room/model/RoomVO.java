2c3a9fc045b71bcb269934802eb3237668a4bfdf
yiting <manana034@hotmail.com>
2021-08-23 23:25:37 +0800

    roomVo

--- src/com/room/model/RoomVO.java
+++ src/com/room/model/RoomVO.java
@@ -2,7 +2,8 @@ package com.room.model;
 
 import java.io.Serializable;
 
-@Data
+import lombok.Data;
+
 public class RoomVO implements Serializable {
 	private String rm_no;
 	private Integer type_no;
@@ -12,5 +13,45 @@ public class RoomVO implements Serializable {
 	
 	public RoomVO() {
 		super();
+	}
+
+	public String getRm_no() {
+		return rm_no;
+	}
+
+	public void setRm_no(String rm_no) {
+		this.rm_no = rm_no;
+	}
+
+	public Integer getType_no() {
+		return type_no;
+	}
+
+	public void setType_no(Integer type_no) {
+		this.type_no = type_no;
+	}
+
+	public String getRm_info() {
+		return rm_info;
+	}
+
+	public void setRm_info(String rm_info) {
+		this.rm_info = rm_info;
+	}
+
+	public Integer getRm_state() {
+		return rm_state;
+	}
+
+	public void setRm_state(Integer rm_state) {
+		this.rm_state = rm_state;
+	}
+
+	public String getName_title() {
+		return name_title;
+	}
+
+	public void setName_title(String name_title) {
+		this.name_title = name_title;
 	}
 }
