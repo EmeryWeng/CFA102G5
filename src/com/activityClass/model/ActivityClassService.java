94e64faddd6219d07da8ef11a92adc04cc62d1c7
xinweiWeng <dybleyer0421@gmail.com>
2021-08-26 23:19:51 +0800

    更改變數名稱

--- src/com/activityClass/model/ActivityClassService.java
+++ src/com/activityClass/model/ActivityClassService.java
@@ -4,31 +4,32 @@ import java.util.List;
 
 public class ActivityClassService {
 	private I_ActivityClassDAO dao;
+	
 	public ActivityClassService() {
 		dao = new ActivityClassJDBCDAO();
 	}
 	
 	
-	public ActivityClassVO addActClass(String actClassName,Boolean actClassState) {
+	public ActivityClassVO addActClass(String act_class_name,Boolean act_class_state) {
 		ActivityClassVO actClassVO = new ActivityClassVO();
-		actClassVO.setAct_class_name(actClassName);
-		actClassVO.setAct_class_state(actClassState);
+		actClassVO.setAct_class_name(act_class_name);
+		actClassVO.setAct_class_state(act_class_state);
 		return dao.insert(actClassVO);
 	}
 	
 	
-	public ActivityClassVO updateActClass(Integer actClassId,String actClassName,Boolean actClassState) {
+	public ActivityClassVO updateActClass(Integer act_class_no,String act_class_name,Boolean act_class_state) {
 		ActivityClassVO actClassVO = new ActivityClassVO();
-		actClassVO.setAct_class_no(actClassId);
-		actClassVO.setAct_class_name(actClassName);
-		actClassVO.setAct_class_state(actClassState);
+		actClassVO.setAct_class_no(act_class_no);
+		actClassVO.setAct_class_name(act_class_name);
+		actClassVO.setAct_class_state(act_class_state);
 		dao.update(actClassVO);
 		
 		return actClassVO;
 	}
 	
-	public ActivityClassVO getActClassByPk(Integer actClassId) {
-		return dao.findByPk(actClassId);
+	public ActivityClassVO getActClassByPk(Integer act_class_no) {
+		return dao.findByPk(act_class_no);
 	}
 	
 	public List<ActivityClassVO> getShowInFrontActClass(){
