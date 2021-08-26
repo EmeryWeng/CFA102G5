a99b87236377e2040c1d15794419a28d661099e0
xinweiWeng <dybleyer0421@gmail.com>
2021-08-27 00:14:16 +0800

    新增ActivityService

---
+++ src/com/activity/model/ActivityService.java
@@ -0,0 +1,52 @@
+package com.activity.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class ActivityService {
+	private I_ActivityDAO dao;
+	
+	public ActivityService() {
+		dao = new ActivityDAO();
+	}
+	
+	public ActivityVO addAct(ActivityVO actVO) {
+		return dao.insert(actVO);
+	}
+	
+	public void updateAct(ActivityVO actVO) {
+		dao.update(actVO);
+	}
+	
+	public ActivityVO getActByPk(Integer act_no){
+		return dao.findByPk(act_no);
+	}
+	
+	public List<ActivityVO> getActByName(String act_name){
+		return dao.findByName(act_name);
+	}
+	
+	public List<ActivityVO> getActByClassNo(Integer act_class_no){
+		return dao.findByActClassNo(act_class_no);
+	}
+	
+	public Integer getActJoinNumberByPk(Integer act_no){
+		return dao.getJoinNumber(act_no);
+	}
+	
+	public List<ActivityVO> getPopularAct(){
+		return dao.getPopularAct();
+	}
+	
+	public List<ActivityVO> ShowActToFront(){
+		return dao.getActToFront();
+	}
+	
+	public List<ActivityVO> getAllAct(){
+		return dao.getAll();
+	}
+	
+	public Map<String,String[]> getActJoinActClass(){
+		return dao.getActJoinActClass();
+	}
+}
