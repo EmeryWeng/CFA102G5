e0e9b8b869003dfcc836aec6ecd8564c2417c75d
xinweiWeng <dybleyer0421@gmail.com>
2021-08-29 15:18:35 +0800

    新增delete方法

--- src/com/activityImage/model/I_ActivityImageDAO.java
+++ src/com/activityImage/model/I_ActivityImageDAO.java
@@ -6,6 +6,7 @@ import java.util.List;
 public interface I_ActivityImageDAO {
 	public ActivityImageVO insert(ActivityImageVO actImageVO);
 	public void update(ActivityImageVO act_image_no);
+	public void delete(Integer act_image_no);
 	public ActivityImageVO findByPk(Integer act_image_no);
 	public List<ActivityImageVO> findByActNo(Integer act_no);
 	public List<ActivityImageVO> getAll();
