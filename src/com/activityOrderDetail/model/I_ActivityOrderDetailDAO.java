eda7b4c6c54657475a2408122116da23b50c4875
xinweiWeng <dybleyer0421@gmail.com>
2021-08-30 00:24:46 +0800

    更改活動訂單明細的表格 多一個流水號 將另外原本是複合主鍵改成FK，做一系列的更動

--- src/com/activityOrderDetail/model/I_ActivityOrderDetailDAO.java
+++ src/com/activityOrderDetail/model/I_ActivityOrderDetailDAO.java
@@ -5,8 +5,9 @@ import java.util.List;
 //狀態
 public interface I_ActivityOrderDetailDAO {
 	public ActivityOrderDetailVO insert(ActivityOrderDetailVO actOrderDetailVO);
-	public void update(ActivityOrderDetailVO actOrderDetailVO,Integer act_order_no,Integer act_session_no);
-	
+	public void update(ActivityOrderDetailVO actOrderDetailVO);
+	//更新 只能針對該活動訂單明細編號 做額外資訊的更改 然後狀態要改成3(已改期)
+	public ActivityOrderDetailVO findByPk(Integer act_order_detail_no);
 	public List<ActivityOrderDetailVO> findByActOrderNo(Integer act_order_no);
 	public List<ActivityOrderDetailVO> findByActSessionNo(Integer act_session_no);
 	public List<ActivityOrderDetailVO> getActOrderDetailState(Integer act_order_detail_state);
