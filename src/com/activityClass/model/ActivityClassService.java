28c24561913fe2bdba082c08b929e73469dfa2c6
xinweiWeng <dybleyer0421@gmail.com>
2021-08-29 17:19:52 +0800

    將原本新增方法有兩個，做修正

--- src/com/activityClass/model/ActivityClassService.java
+++ src/com/activityClass/model/ActivityClassService.java
@@ -16,18 +16,20 @@ public class ActivityClassService {
 		ActivityClassVO vo = new ActivityClassVO();
 		vo.setAct_class_name(act_class_name);
 		vo.setAct_class_state(new Boolean(act_class_state));
+		
 		return dao.insert(vo);
 	}
 	
-	public ActivityClassVO addActClass(String act_class_no,
+	public void updateActClass(String act_class_no,
 			String act_class_name,String act_class_state) {
 		
 		ActivityClassVO vo = new ActivityClassVO();
+		
 		vo.setAct_class_no(new Integer(act_class_no));
 		vo.setAct_class_name(act_class_name);
 		vo.setAct_class_state(new Boolean(act_class_state));
 		
-		return dao.insert(vo);
+		dao.update(vo);
 	}
 	
 	public ActivityClassVO getActClassByPk(String act_class_no) {
