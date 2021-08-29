c66ba6d2fc11d1a6db2d9ea0eb601d9f186b31a6
xinweiWeng <dybleyer0421@gmail.com>
2021-08-29 16:45:52 +0800

    重新修改方法 在裡面封裝物件

--- src/com/activity/model/ActivityService.java
+++ src/com/activity/model/ActivityService.java
@@ -2,6 +2,7 @@ package com.activity.model;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ActivityService {
 	private I_ActivityDAO dao;
@@ -10,36 +11,80 @@ public class ActivityService {
 		dao = new ActivityDAO();
 	}
 	
-	public ActivityVO addAct(ActivityVO actVO) {
-		return dao.insert(actVO);
+	public ActivityVO addAct(String act_class_no,String act_name,
+			String act_price,String act_location,String act_schedule_time,
+			String act_instruction,String act_gather_location,
+			String act_location_longitude,String act_location_latitude,
+			String act_sell_number,String act_join_number,
+			String act_evaluation_number,String act_average_star_number,
+			String act_state) {
+		
+		ActivityVO vo = new ActivityVO();
+		vo.setAct_class_no(new Integer(act_class_no));
+		vo.setAct_name(act_name);
+		vo.setAct_price(new Integer(act_price));
+		vo.setAct_location(act_location);
+		vo.setAct_schedule_time(new Integer(act_schedule_time));
+		vo.setAct_instruction(act_instruction);
+		vo.setAct_gather_location(act_gather_location);
+		vo.setAct_location_longitude(new Double(act_location_longitude));
+		vo.setAct_location_latitude(new Double(act_location_latitude));
+		vo.setAct_sell_number(new Integer(act_sell_number));
+		vo.setAct_join_number(new Integer(act_join_number));
+		vo.setAct_evaluation_number(new Integer(act_evaluation_number));
+		vo.setAct_average_star_number(new Double(act_average_star_number));
+		vo.setAct_state(new Boolean(act_state));
+		return dao.insert(vo);
 	}
 	
-	public void updateAct(ActivityVO actVO) {
-		dao.update(actVO);
+	public void updateAct(String act_no,String act_class_no,String act_name,
+			String act_price,String act_location,String act_schedule_time,
+			String act_instruction,String act_gather_location,
+			String act_location_longitude,String act_location_latitude,
+			String act_sell_number,String act_join_number,
+			String act_evaluation_number,String act_average_star_number,
+			String act_state) {
+		
+		ActivityVO vo = new ActivityVO();
+		vo.setAct_no(new Integer(act_no));
+		vo.setAct_class_no(new Integer(act_class_no));
+		vo.setAct_name(act_name);
+		vo.setAct_price(new Integer(act_price));
+		vo.setAct_location(act_location);
+		vo.setAct_schedule_time(new Integer(act_schedule_time));
+		vo.setAct_instruction(act_instruction);
+		vo.setAct_gather_location(act_gather_location);
+		vo.setAct_location_longitude(new Double(act_location_longitude));
+		vo.setAct_location_latitude(new Double(act_location_latitude));
+		vo.setAct_sell_number(new Integer(act_sell_number));
+		vo.setAct_join_number(new Integer(act_join_number));
+		vo.setAct_evaluation_number(new Integer(act_evaluation_number));
+		vo.setAct_average_star_number(new Double(act_average_star_number));
+		vo.setAct_state(new Boolean(act_state));
+		
+		dao.insert(vo);
 	}
 	
-	public ActivityVO getActByPk(Integer act_no){
-		return dao.findByPk(act_no);
+	public ActivityVO getActByPk(String act_no){
+		return dao.findByPk(new Integer(act_no));
 	}
 	
 	public List<ActivityVO> getActByName(String act_name){
 		return dao.findByName(act_name);
 	}
 	
-	public List<ActivityVO> getActByClassNo(Integer act_class_no){
-		return dao.findByActClassNo(act_class_no);
+	public List<ActivityVO> getActByClassNo(String act_class_no){
+		return dao.findByActClassNo(new Integer(act_class_no));
 	}
 	
 	public List<ActivityVO> getPopularAct(){
 		return dao.getPopularAct();
 	}
-	//§W¨[™¨∫A = true(1)
-	public List<ActivityVO> getShowActToFront(){
-		return dao.getActToFront();
-	}
-	
-	public List<ActivityVO> getAllAct(){
-		return dao.getAll();
+	//ÂèñÂæó‰∏äÊû∂Ê¥ªÂãï
+	public List<ActivityVO> getAll(){
+		return dao.getAll().stream()
+				.filter(act -> act.getAct_state() == true)
+				.collect(Collectors.toList());
 	}
 	
 }
