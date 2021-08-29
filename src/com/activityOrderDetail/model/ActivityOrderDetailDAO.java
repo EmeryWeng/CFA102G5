e62b577873d05e47b259ce4e77d11fa64eda3f3d
xinweiWeng <dybleyer0421@gmail.com>
2021-08-30 00:22:33 +0800

    新增活動訂單明細連線池版

---
+++ src/com/activityOrderDetail/model/ActivityOrderDetailDAO.java
@@ -0,0 +1,237 @@
+package com.activityOrderDetail.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
+
+public class ActivityOrderDetailDAO implements I_ActivityOrderDetailDAO {
+	private static DataSource ds;
+	
+	private final String[] GET_KEY = {"act_order_detail_no"};
+	private final String SELECT_All_SQL = "SELECT * FROM ACTIVITY_ORDER_DETAIL";
+	private final String INSERT_SQL = "INSERT INTO ACTIVITY_ORDER_DETAIL VALUES(?,?,?,?,?,?,?,?)";
+	private final String UPDATE_SQL = "UPDATE ACTIVITY_ORDER_DETAIL SET act_order_no = ?,act_session_no = ?,act_real_join_number = ?,act_order_price = ?,act_coupon_price = ? "
+			+ ",act_price_total = ?,act_order_detail_state = ? WHERE act_order_detail_no = ?";
+	private final String SELECT_BY_PK_SQL = "SELECT * FROM ACTIVITY_ORDER_DETAIL WHERE act_order_detail_no = ?";
+	private final String SELECT_BY_ACTIVITY_ORDER_NO_SQL = "SELECT * FROM ACTIVITY_ORDER_DETAIL WHERE act_order_no = ?";
+	private final String SELECT_BY_ACTIVITY_SESSION_NO_SQL = "SELECT * FROM ACTIVITY_ORDER_DETAIL WHERE act_session_no = ?";
+	private final String SELECT_BY_ACTIVITY_ORDER_DETAIL_STATE_SQL = "SELECT * FROM ACTIVITY_ORDER_DETAIL WHERE act_order_detail_state = ?";
+	
+	static {	
+		try {
+			ds = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/TestDB");
+		}catch(NamingException ex) {
+			ex.printStackTrace();
+		}	
+	}
+	
+	
+	@Override
+	public ActivityOrderDetailVO insert(ActivityOrderDetailVO actOrderDetailVO) {
+		ResultSet rs = null;
+		try (Connection con = ds.getConnection()) {
+			
+			PreparedStatement ps = con.prepareStatement(INSERT_SQL, GET_KEY);
+			ps.setString(1,null);
+			ps.setInt(2,actOrderDetailVO.getAct_order_no());
+			ps.setInt(3,actOrderDetailVO.getAct_session_no());
+			ps.setInt(4,actOrderDetailVO.getAct_real_join_number());
+			ps.setInt(5,actOrderDetailVO.getAct_order_price());
+			ps.setInt(6,actOrderDetailVO.getAct_coupon_price());
+			ps.setInt(7,actOrderDetailVO.getAct_price_total());
+			ps.setInt(8,actOrderDetailVO.getAct_order_detail_state());
+			ps.executeUpdate();
+
+			rs = ps.getGeneratedKeys(); //綁定主鍵值，這樣撈回來才有正確的Id
+			if (rs.next()) {
+				actOrderDetailVO.setAct_order_detail_no(rs.getInt(1));
+			}
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} 
+		
+		return actOrderDetailVO;
+	}
+
+	@Override
+	public void update(ActivityOrderDetailVO actOrderDetailVO) {
+		try (Connection con = ds.getConnection()) {
+			
+			PreparedStatement ps = con.prepareStatement(UPDATE_SQL);
+			ps.setInt(1,actOrderDetailVO.getAct_order_no());
+			ps.setInt(2,actOrderDetailVO.getAct_session_no());
+			ps.setInt(3,actOrderDetailVO.getAct_real_join_number());
+			ps.setInt(4,actOrderDetailVO.getAct_order_price());
+			ps.setInt(5,actOrderDetailVO.getAct_coupon_price());
+			ps.setInt(6,actOrderDetailVO.getAct_price_total());
+			ps.setInt(7,actOrderDetailVO.getAct_order_detail_state());
+			ps.setInt(8,actOrderDetailVO.getAct_order_detail_no()); 
+			
+			ps.executeUpdate();
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} 
+	}
+
+	@Override
+	public ActivityOrderDetailVO findByPk(Integer act_order_detail_no) {
+		ActivityOrderDetailVO actOrderDetailVO = null;
+		ResultSet rs = null;
+		try (Connection con = ds.getConnection()) {
+			
+			PreparedStatement ps = con.prepareStatement(SELECT_BY_PK_SQL);
+			ps.setInt(1,act_order_detail_no);
+			rs = ps.executeQuery();	
+			while (rs.next()) {
+				actOrderDetailVO = new ActivityOrderDetailVO();
+				actOrderDetailVO.setAct_order_detail_no(rs.getInt(1));
+				actOrderDetailVO.setAct_order_no(rs.getInt(2));
+				actOrderDetailVO.setAct_session_no(rs.getInt(3));
+				actOrderDetailVO.setAct_real_join_number(rs.getInt(4));
+				actOrderDetailVO.setAct_order_price(rs.getInt(5));
+				actOrderDetailVO.setAct_coupon_price(rs.getInt(6));
+				actOrderDetailVO.setAct_price_total(rs.getInt(7));
+				actOrderDetailVO.setAct_order_detail_state(rs.getInt(8));
+			}
+	
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} 
+		
+		return actOrderDetailVO;
+	}
+
+	@Override
+	public List<ActivityOrderDetailVO> findByActOrderNo(Integer act_order_no) {
+		List<ActivityOrderDetailVO> list = new ArrayList<>();
+		ActivityOrderDetailVO actOrderDetailVO = null;
+		ResultSet rs = null;
+		try (Connection con = ds.getConnection()) {
+			
+			PreparedStatement ps = con.prepareStatement(SELECT_BY_ACTIVITY_ORDER_NO_SQL);
+			ps.setInt(1, act_order_no);
+			rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				actOrderDetailVO = new ActivityOrderDetailVO();
+				actOrderDetailVO.setAct_order_detail_no(rs.getInt(1));
+				actOrderDetailVO.setAct_order_no(rs.getInt(2));
+				actOrderDetailVO.setAct_session_no(rs.getInt(3));
+				actOrderDetailVO.setAct_real_join_number(rs.getInt(4));
+				actOrderDetailVO.setAct_order_price(rs.getInt(5));
+				actOrderDetailVO.setAct_coupon_price(rs.getInt(6));
+				actOrderDetailVO.setAct_price_total(rs.getInt(7));
+				actOrderDetailVO.setAct_order_detail_state(rs.getInt(8));
+				list.add(actOrderDetailVO);
+			}
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} 
+		
+		return list;
+	}
+
+	@Override
+	public List<ActivityOrderDetailVO> findByActSessionNo(Integer act_session_no) {
+		List<ActivityOrderDetailVO> list = new ArrayList<>();
+		ActivityOrderDetailVO actOrderDetailVO = null;
+		ResultSet rs = null;
+		try (Connection con = ds.getConnection()) {
+			
+			PreparedStatement ps = con.prepareStatement(SELECT_BY_ACTIVITY_SESSION_NO_SQL);
+			ps.setInt(1, act_session_no);
+			rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				actOrderDetailVO = new ActivityOrderDetailVO();
+				actOrderDetailVO.setAct_order_detail_no(rs.getInt(1));
+				actOrderDetailVO.setAct_order_no(rs.getInt(2));
+				actOrderDetailVO.setAct_session_no(rs.getInt(3));
+				actOrderDetailVO.setAct_real_join_number(rs.getInt(4));
+				actOrderDetailVO.setAct_order_price(rs.getInt(5));
+				actOrderDetailVO.setAct_coupon_price(rs.getInt(6));
+				actOrderDetailVO.setAct_price_total(rs.getInt(7));
+				actOrderDetailVO.setAct_order_detail_state(rs.getInt(8));
+				list.add(actOrderDetailVO);
+			}
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} 
+		
+		return list;
+	}
+
+	@Override
+	public List<ActivityOrderDetailVO> getActOrderDetailState(Integer act_order_detail_state) {
+		List<ActivityOrderDetailVO> list = new ArrayList<>();
+		ActivityOrderDetailVO actOrderDetailVO = null;
+		ResultSet rs = null;
+		try (Connection con = ds.getConnection()) {
+			
+			PreparedStatement ps = con.prepareStatement(SELECT_BY_ACTIVITY_ORDER_DETAIL_STATE_SQL);
+			
+			ps.setInt(1,act_order_detail_state);
+			rs = ps.executeQuery();	
+			while (rs.next()) {
+				actOrderDetailVO = new ActivityOrderDetailVO();
+				actOrderDetailVO.setAct_order_detail_no(rs.getInt(1));
+				actOrderDetailVO.setAct_order_no(rs.getInt(2));
+				actOrderDetailVO.setAct_session_no(rs.getInt(3));
+				actOrderDetailVO.setAct_real_join_number(rs.getInt(4));
+				actOrderDetailVO.setAct_order_price(rs.getInt(5));
+				actOrderDetailVO.setAct_coupon_price(rs.getInt(6));
+				actOrderDetailVO.setAct_price_total(rs.getInt(7));
+				actOrderDetailVO.setAct_order_detail_state(rs.getInt(8));
+				list.add(actOrderDetailVO);
+			}
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} 
+		
+		return list;
+	}
+
+	@Override
+	public List<ActivityOrderDetailVO> getAll() {
+		List<ActivityOrderDetailVO> list = new ArrayList<>();
+		ActivityOrderDetailVO actOrderDetailVO = null;
+		ResultSet rs = null;
+		try (Connection con = ds.getConnection()) {
+			
+			PreparedStatement ps = con.prepareStatement(SELECT_All_SQL);
+			rs = ps.executeQuery();	
+			while (rs.next()) {
+				actOrderDetailVO = new ActivityOrderDetailVO();
+				actOrderDetailVO.setAct_order_detail_no(rs.getInt(1));
+				actOrderDetailVO.setAct_order_no(rs.getInt(2));
+				actOrderDetailVO.setAct_session_no(rs.getInt(3));
+				actOrderDetailVO.setAct_real_join_number(rs.getInt(4));
+				actOrderDetailVO.setAct_order_price(rs.getInt(5));
+				actOrderDetailVO.setAct_coupon_price(rs.getInt(6));
+				actOrderDetailVO.setAct_price_total(rs.getInt(7));
+				actOrderDetailVO.setAct_order_detail_state(rs.getInt(8));
+				list.add(actOrderDetailVO);
+			}
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} 
+		
+		return list;
+	}
+
+}
