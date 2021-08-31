605b2c9a2d850cb5bcca37ad53dbc028d5911e45
f79483127 <s1216430@gmail.com>
2021-08-31 15:59:30 +0800

    新增店家照片service

---
+++ src/com/foodImg/model/FoodImgService.java
@@ -0,0 +1,42 @@
+package com.foodImg.model;
+
+import java.util.List;
+
+
+public class FoodImgService {
+	private I_FoodImgDAO dao;
+	
+	public FoodImgService() {
+		dao = new FoodImgDAO();
+	}
+	
+	public FoodImgVO addImg(Integer fd_no,byte[] fd_img) {
+
+		FoodImgVO foodImgVO = new FoodImgVO();
+		foodImgVO.setFd_no(fd_no);
+		foodImgVO.setFd_img(fd_img);
+
+		dao.insert(foodImgVO);
+		return foodImgVO;
+	}
+	
+	public FoodImgVO updateImg(Integer fd_no,byte[] fd_img,Integer fd_img_no) {
+
+		FoodImgVO foodImgVO = new FoodImgVO();
+		foodImgVO.setFd_no(fd_no);
+		foodImgVO.setFd_img(fd_img);
+		foodImgVO.setFd_img_no(fd_img_no);
+
+		dao.update(foodImgVO);
+		return foodImgVO;
+	}
+	
+	public List<FoodImgVO> foodImg(Integer fd_no){
+		return dao.findimgByFK(fd_no);
+	}
+	
+	public List<FoodImgVO> allImg(){
+		return dao.getAllImg();
+	}
+	
+}
