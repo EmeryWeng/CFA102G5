b4f77dffd3e429aa4992f2a204d9b2694f932a63
xinweiWeng <dybleyer0421@gmail.com>
2021-09-07 00:33:26 +0800

    新增活動類別狀態切換的方法(介面)

--- src/com/activityClass/model/I_ActivityClassDAO.java
+++ src/com/activityClass/model/I_ActivityClassDAO.java
@@ -6,5 +6,6 @@ public interface I_ActivityClassDAO {
 	public ActivityClassVO insert(ActivityClassVO actClassVO);
 	public void update(ActivityClassVO actClassVO);
 	public ActivityClassVO findByPk(Integer act_class_no);
+	public void switchActivityClassState(Integer act_class_no,Boolean act_class_state);
 	public List<ActivityClassVO> getAll();
 }
