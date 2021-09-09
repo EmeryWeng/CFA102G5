e7209a8b4fbbc93c748de891a77a9d7920ae33bd
f79483127 <s1216430@gmail.com>
2021-09-09 20:38:05 +0800

    部門.員工.店家照片.店家 controller

---
+++ src/com/foodImg/controller/FoodImgServlet.java
@@ -0,0 +1,196 @@
+package com.foodImg.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import com.foodImg.model.FoodImgService;
+import com.foodImg.model.FoodImgVO;
+@MultipartConfig(fileSizeThreshold = 1024 * 1024, maxFileSize = 5 * 1024 * 1024, maxRequestSize = 5 * 5 * 1024 * 1024)
+public class FoodImgServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		
+		if("getOneFoodStoreImg".equals(action)) {	// 來自select_page.jsp的請求
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			try {
+			Integer fd_no = new Integer(req.getParameter("fd_no").trim());
+			/*************************** 2.開始查詢資料 *****************************************/
+			FoodImgService imgSvc = new FoodImgService();
+			List<FoodImgVO> imgVO = imgSvc.foodImg(fd_no);
+			
+			
+			if(imgVO.size()==0) {
+				errorMsgs.add("查無此店家或無照片");
+			}
+			
+			/*************************** 3.查詢完成,準備轉交(Send the Success view) *************/
+			req.setAttribute("imgVO", imgVO);
+			String url = "back_end/foodimg/getOneFoodStoreImg.jsp";
+			RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交
+			successView.forward(req, res);
+			return;
+			/*************************** 其他可能的錯誤處理 *************************************/
+			}catch (Exception e) {
+				errorMsgs.add("查無此店家或圖片或輸入錯誤"+e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("back_end/foodimg/select_page.jsp");
+				failureView.forward(req, res);
+			}
+		}
+		
+		
+		
+		
+		if("addImg".equals(action)) {				// 來自select_page.jsp的請求
+			
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			try {
+			byte[] img = null;
+			/***************************1.接收請求參數 -**********************/
+			Integer fd_no = new Integer(req.getParameter("fd_no").trim());
+			
+			Part fd_img = req.getPart("fd_img");
+			/***************************2.圖片處理**********************/
+			InputStream in = fd_img.getInputStream();
+			img = new byte[in.available()];
+			in.read(img);
+			in.close();
+			/***************************3.開始新增資料***************************************/
+			FoodImgVO foodImgVO = new FoodImgVO();
+			foodImgVO.setFd_no(fd_no);
+			foodImgVO.setFd_img(img);
+			FoodImgService imgSvc = new FoodImgService();
+			foodImgVO = imgSvc.addImg(fd_no, img);
+			/***************************4.新增完成,準備轉交(Send the Success view)***********/
+			String url ="back_end/foodimg/allStoreImg.jsp";
+			RequestDispatcher successView = req.getRequestDispatcher(url);
+			successView.forward(req, res);
+		}catch (Exception e) {
+			errorMsgs.add("新增失敗 請填寫正確店家編號"+e.getMessage());
+			RequestDispatcher failureView = req
+					.getRequestDispatcher("back_end/foodimg/select_page.jsp");
+			failureView.forward(req, res);
+		}
+		}
+		
+		
+		if ("delete".equals(action)) { // 來自listAllEmp.jsp
+
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+	
+			try {
+				/***************************1.接收請求參數***************************************/
+				Integer fd_img_no = new Integer(req.getParameter("fd_img_no"));
+				
+				/***************************2.開始刪除資料***************************************/
+				FoodImgService Svc = new FoodImgService();
+				Svc.deleteAcc(fd_img_no);
+				
+				/***************************3.刪除完成,準備轉交(Send the Success view)***********/								
+				String url = "back_end/foodimg/allStoreImg.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url);// 刪除成功後,轉交回送出刪除的來源網頁
+				successView.forward(req, res);
+				
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (Exception e) {
+				errorMsgs.add("刪除資料失敗:"+e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("select_page.jsp");
+				failureView.forward(req, res);
+			}
+		}
+		
+		if ("One_Img_Update".equals(action)) { 
+		
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+
+			try {
+				Integer fd_img_no = new Integer(req.getParameter("fd_img_no"));
+
+				FoodImgService Svc = new FoodImgService();
+				FoodImgVO imgvo = Svc.findByPk(fd_img_no);
+			
+				req.setAttribute("imgvo", imgvo);
+				
+				String url = "back_end/foodimg/updateFoodImg.jsp";
+  				RequestDispatcher successView = req.getRequestDispatcher(url); 
+  				successView.forward(req, res);
+				}catch (Exception e) {
+					errorMsgs.add("修改資料失敗:" + e.getMessage());
+						e.printStackTrace();
+							RequestDispatcher failureView = req
+									.getRequestDispatcher("back_end/foodimg/select_page.jsp");
+							failureView.forward(req, res);
+							}
+		}
+		
+		
+		if ("update".equals(action)) {
+			
+		
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+		try {	
+			Integer fd_img_no = new Integer(req.getParameter("fd_img_no"));
+			Integer fd_no = new Integer(req.getParameter("fd_no"));
+			Part fd_img = req.getPart("fd_img");
+			InputStream img = fd_img.getInputStream();
+			if(img.available()==0) {
+				
+			}
+			byte[] Pic = new byte[img.available()];
+			img.read(Pic);
+			img.close();
+
+			FoodImgVO vo = new FoodImgVO();
+			vo.setFd_img_no(fd_img_no);
+			vo.setFd_no(fd_no);
+			vo.setFd_img(Pic);
+		
+			FoodImgService Svc = new FoodImgService();
+			vo = Svc.updateImg(fd_no, Pic, fd_img_no);
+			
+			req.setAttribute("vo", vo); 
+			String url = "back_end/foodimg/allStoreImg.jsp";
+			RequestDispatcher successView = req.getRequestDispatcher(url);
+			successView.forward(req, res);
+			
+			}catch (Exception e) {
+				errorMsgs.add("修改資料失敗:" + e.getMessage());
+					e.printStackTrace();
+						RequestDispatcher failureView = req
+								.getRequestDispatcher("back_end/foodimg/select_page.jsp");
+						failureView.forward(req, res);
+			}
+		
+	
+		
+	}
+}
+
+}
---
+++ src/com/foodStore/controller/FoodStoreServlet.java
@@ -0,0 +1,184 @@
+package com.foodStore.controller;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.foodStore.model.*;
+
+public class FoodStoreServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public FoodStoreServlet() {
+        super();
+    }
+
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		
+		if("Update_One".equals(action)) {
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+			Integer fd_no = new Integer(req.getParameter("fd_no"));
+
+			FoodStoreService ser = new FoodStoreService();
+			FoodStoreVO vo = ser.getOneStore(fd_no);
+			req.setAttribute("vo", vo);
+			
+			String url = "back_end/foodStore/updateStore.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); 
+				successView.forward(req, res);
+		}
+	
+	
+	
+		if("update".equals(action)) {
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			try {
+			Integer fd_no = new Integer(req.getParameter("fd_no"));
+			Integer fd_class_no = new Integer(req.getParameter("fd_class_no"));
+			String fd_name = req.getParameter("fd_name");	
+			String fd_address = req.getParameter("fd_address");
+			
+			String LONGITUDE = "^[\\-\\+]?(0(\\.\\d{1,6})?|([1-9](\\d)?)(\\.\\d{1,6})?|1[0-7]\\d{1}(\\.\\d{1,6})?|180(\\.0{1,6})?)$";
+			String fd_longitude1 = req.getParameter("fd_longitude");
+			if (fd_longitude1 == null || fd_longitude1.trim().length() == 0) {
+				errorMsgs.add("經度: 請勿空白");
+			} else if(!fd_longitude1.trim().matches(LONGITUDE)) { 
+				errorMsgs.add("經度: 請依照格式輸入");
+            }
+			Double fd_longitude = new Double(fd_longitude1);    //正則表示後轉型成DOUBLE
+			
+		    String LATITUDE = "^[\\-\\+]?((0|([1-8]\\d?))(\\.\\d{1,6})?|90(\\.0{1,6})?)$";
+		    String fd_latitude1 = req.getParameter("fd_latitude");
+		    if (fd_latitude1 == null || fd_latitude1.trim().length() == 0) {
+				errorMsgs.add("緯度: 請勿空白");
+			} else if(!fd_latitude1.trim().matches(LATITUDE)) { 
+				errorMsgs.add("緯度: 請依照格式輸入");
+            }
+			Double fd_latitude = new Double(fd_latitude1);		//正則表示後轉型成DOUBLE
+			
+			String fd_service = req.getParameter("fd_service");
+			
+			Boolean fd_state = new Boolean(req.getParameter("fd_state"));
+			if (!errorMsgs.isEmpty()) {
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/back_end/foodStore/allStore.jsp");
+				failureView.forward(req, res);
+				return;
+			}
+			FoodStoreService ser = new FoodStoreService();
+			FoodStoreVO vo = ser.updateFoodStore(fd_name, fd_address, fd_longitude, fd_latitude, fd_service, fd_state, fd_class_no, fd_no);
+		
+			req.setAttribute("vo", vo); // 資料庫update成功後,正確的的vo物件,存入req
+			String url = "/back_end/foodStore/allStore.jsp";
+			RequestDispatcher successView = req.getRequestDispatcher(url); // 修改成功後,轉交listOneEmp.jsp
+			successView.forward(req, res);
+
+			/***************************其他可能的錯誤處理*************************************/
+		} catch (Exception e) {
+			errorMsgs.add("修改資料失敗:"+e.getMessage());
+			RequestDispatcher failureView = req
+					.getRequestDispatcher("/back_end/foodStore/allStore.jsp");
+			failureView.forward(req, res);
+		}
+	
+		}
+		
+		if("insert".equals(action))	{
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			try {
+			Integer fd_class_no = new Integer(req.getParameter("fd_class_no"));
+			String fd_name = req.getParameter("fd_name");	
+			String fd_address = req.getParameter("fd_address");
+			
+			String LONGITUDE = "^[\\-\\+]?(0(\\.\\d{1,6})?|([1-9](\\d)?)(\\.\\d{1,6})?|1[0-7]\\d{1}(\\.\\d{1,6})?|180(\\.0{1,6})?)$";
+			String fd_longitude1 = req.getParameter("fd_longitude");
+			if (fd_longitude1 == null || fd_longitude1.trim().length() == 0) {
+				errorMsgs.add("經度: 請勿空白");
+			} else if(!fd_longitude1.trim().matches(LONGITUDE)) { 
+				errorMsgs.add("經度: 請依照格式輸入");
+            }
+			Double fd_longitude = new Double(fd_longitude1);    //正則表示後轉型成DOUBLE
+			
+		    String LATITUDE = "^[\\-\\+]?((0|([1-8]\\d?))(\\.\\d{1,6})?|90(\\.0{1,6})?)$";
+		    String fd_latitude1 = req.getParameter("fd_latitude");
+		    if (fd_latitude1 == null || fd_latitude1.trim().length() == 0) {
+				errorMsgs.add("緯度: 請勿空白");
+			} else if(!fd_latitude1.trim().matches(LATITUDE)) { 
+				errorMsgs.add("緯度: 請依照格式輸入");
+            }
+			Double fd_latitude = new Double(fd_latitude1);		//正則表示後轉型成DOUBLE
+			
+			String fd_service = req.getParameter("fd_service");
+			
+			Boolean fd_state = new Boolean(req.getParameter("fd_state"));
+			if (!errorMsgs.isEmpty()) {
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/back_end/foodStore/allStore.jsp");
+				failureView.forward(req, res);
+				return;
+			}
+			FoodStoreService ser = new FoodStoreService();
+			FoodStoreVO vo = ser.addFoodStore(fd_name, fd_address, fd_longitude, fd_latitude, fd_service, fd_state, fd_class_no);
+			
+			req.setAttribute("vo", vo); // 資料庫update成功後,正確的的vo物件,存入req
+			String url = "/back_end/foodStore/allStore.jsp";
+			RequestDispatcher successView = req.getRequestDispatcher(url); // 修改成功後,轉交listOneEmp.jsp
+			successView.forward(req, res);
+
+			/***************************其他可能的錯誤處理*************************************/
+		} catch (Exception e) {
+			errorMsgs.add("修改資料失敗:"+e.getMessage());
+			RequestDispatcher failureView = req
+					.getRequestDispatcher("/back_end/foodStore/allStore.jsp");
+			failureView.forward(req, res);
+		}
+		}
+	
+		
+		
+		if("getStoreFK".equals(action)) {
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+			try {
+			Integer fd_class_no = new Integer(req.getParameter("fd_class_no"));
+			FoodStoreService ser = new FoodStoreService();
+			List<FoodStoreVO> storeVO = ser.findfdByFK(fd_class_no);
+			
+			if(storeVO.size()==0) {
+				errorMsgs.add("查無此類別店家");
+			}	
+			if (!errorMsgs.isEmpty()) {
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/back_end/foodStore/allStore.jsp");
+				failureView.forward(req, res);
+				return;
+			}
+			req.setAttribute("storeVO", storeVO);
+			String url = "back_end/foodStore/getStoreFK.jsp";
+			RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交
+			successView.forward(req, res);
+			return;
+			
+			}catch (Exception e) {
+				errorMsgs.add("查詢資料失敗:"+e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/back_end/foodStore/store_page.jsp");
+				failureView.forward(req, res);
+			}
+		}
+	}
+
+}
