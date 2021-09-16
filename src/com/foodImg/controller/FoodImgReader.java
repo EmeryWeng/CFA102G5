a3d22e32aaa5f4b8d22e3063689ed72e65ef4e4d
f79483127 <s1216430@gmail.com>
2021-09-16 23:53:26 +0800

    沒改

--- src/com/foodImg/controller/FoodImgReader.java
+++ src/com/foodImg/controller/FoodImgReader.java
@@ -1,37 +1,28 @@
 package com.foodImg.controller;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.foodImg.model.FoodImgService;
-import com.foodImg.model.FoodImgVO;
 
 
-@WebServlet("/FoodImgReader")
 public class FoodImgReader extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("image/jpeg");
-		Integer fd_img_no = new Integer(req.getParameter("fd_img_no").trim());
-		
 		FoodImgService vic = new FoodImgService();
-		
 		ServletOutputStream out = res.getOutputStream();
+		
+		Integer fd_img_no = new Integer(req.getParameter("fd_img_no").trim());
 	    out.write(vic.findByPk(fd_img_no).getFd_img());
 	    out.close();
 		  	
 	}
-
-
 }
